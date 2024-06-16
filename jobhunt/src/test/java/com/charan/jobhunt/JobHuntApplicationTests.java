41ad5b1e088206cd7cc8197f4842cd1c773e44bd
Charan <charankulal0241@gmail.com>
2024-06-16 13:45:58 +0530

    Connected the applicaton wth the mongodb

--- jobhunt/src/test/java/com/charan/jobhunt/JobHuntApplicationTests.java
+++ jobhunt/src/test/java/com/charan/jobhunt/JobHuntApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JobhuntApplicationTests {
+class JobHuntApplicationTests {
 
 	@Test
 	void contextLoads() {
